0d48e01cc62ee5bab0cf32bf2fcd952c16077887
Rabbit <dimkazayats@gmail.com>
2020-09-14 17:53:35 +0300

    Добавил проект . Сделал таргет ios 12

---
+++ fintech_chat/fintech_chat/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  fintech_chat
+//
+//  Created by Admin on 9/14/20.
+//  Copyright © 2020 Admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .red
+    }
+
+
+}
+
