a49a04437c6b2734b5c0ca9ec41ab3db9b3d2649
Rabbit13245 <dimkazayats@gmail.com>
2020-09-14 23:01:04 +0300

    Выпилил sceneDelegate, Добавил параметр в target для логов. Залогировал методы

--- fintech_chat/fintech_chat/ViewController.swift
+++ fintech_chat/fintech_chat/ViewController.swift
@@ -10,10 +10,55 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let needLog = Bundle.main.object(forInfoDictionaryKey: "NeedLog") as! String
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if (needLog == "YES"){
+            print(#function)
+        }
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if (needLog == "YES"){
+            print(#function)
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        if (needLog == "YES"){
+            print(#function)
+        }
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if (needLog == "YES"){
+            print(#function)
+        }
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        if (needLog == "YES"){
+            print(#function)
+        }
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        if (needLog == "YES"){
+            print(#function)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
+       
         view.backgroundColor = .red
     }
 
