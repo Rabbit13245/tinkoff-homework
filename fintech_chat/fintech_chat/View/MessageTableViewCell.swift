d688697324654a413a6e413e1e53615827d923ba
Rabbit13245 <dimkazayats@gmail.com>
2020-09-28 01:02:01 +0300

    Добавил UiView, в которые надо будет вложить текст сообщения

--- fintech_chat/fintech_chat/View/MessageTableViewCell.swift
+++ fintech_chat/fintech_chat/View/MessageTableViewCell.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class MessageTableViewCell: UITableViewCell {
 
-    @IBOutlet weak var messageLabel: UILabel!
+    //@IBOutlet weak var messageLabel: UILabel!
     
     lazy var messageTextLabel: UILabel = {
         let label = UILabel()
@@ -20,10 +20,21 @@ class MessageTableViewCell: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        
+    
+    }
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+            print("FF")
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        print("DD")
+     
         configureUI()
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
     }
@@ -31,20 +42,22 @@ class MessageTableViewCell: UITableViewCell {
     // MARK: - Private functions
     
     private func configureUI(){
+//        let view = UIView(frame: CGRect(x: 0, y: 0, width: self.contentView.frame.width , height: self.contentView.bounds.height))
         let view = UIView()
-        self.addSubview(view)
         view.backgroundColor = .red
+        
+        self.contentView.addSubview(view)
         
         //view.addSubview(messageTextLabel)
         
         view.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             view.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor, constant: 8),
-            view.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 8),
-            view.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 8),
-            view.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 8),
+            view.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -8),
+            view.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 8),
+            view.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -8),
+            //view.widthAnchor.constraint(equalTo: contentView.widthAnchor)
         ])
-        
         //self.backgroundColor = UIColor.lightGray
     }
 }
