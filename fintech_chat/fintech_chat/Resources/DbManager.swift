b1639e88cadd2d452aaab3e103690b3162668169
Rabbit13245 <dimkazayats@gmail.com>
2020-10-18 22:23:47 +0300

    Добавил dbManager

--- fintech_chat/fintech_chat/Resources/DbManager.swift
+++ fintech_chat/fintech_chat/Resources/DbManager.swift
@@ -1,9 +1,32 @@
-//
-//  DbManager.swift
-//  fintech_chat
-//
-//  Created by Макбук on 18.10.2020.
-//  Copyright © 2020 Admin. All rights reserved.
-//
-
 import Foundation
+import Firebase
+
+class DbManager{
+    
+    public static let shared: DbManager = {
+        let dbManager = DbManager()
+        return dbManager
+    }()
+    
+    private init(){}
+    
+    private let database = Firestore.firestore()
+}
+
+// MARK: - Channels
+extension DbManager{
+    public func getAllChannels(){
+        database.collection("channels").getDocuments { (querySnapshot, error) in
+            if let error = error{
+                print("Fuck you")
+            } else{
+                if let snapshot = querySnapshot {
+                    print(snapshot.documents.count)
+                }
+                else{
+                    print("empty")
+                }
+            }
+        }
+    }
+}
