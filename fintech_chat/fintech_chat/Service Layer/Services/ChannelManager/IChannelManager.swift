ccd7af365f20da3b12883bb66882193994ddd1b8
Rabbit13245 <dimkazayats@gmail.com>
2020-11-12 01:27:39 +0300

    Протоколы для messageManaer, channelManager

--- fintech_chat/fintech_chat/Service Layer/Services/ChannelManager/IChannelManager.swift
+++ fintech_chat/fintech_chat/Service Layer/Services/ChannelManager/IChannelManager.swift
@@ -1,5 +1,18 @@
 import Foundation
 
 protocol IChannelManager {
+    // func getChannel(with id: String, in context: NSManagedObjectContext? = nil) -> ChannelDb?
+    func getChannel(with id: String) -> ChannelDb?
     
+    /// Добавить новые каналы
+    func addNewChannels(_ channels: [Channel])
+    
+    /// Обновить существующие каналы
+    func modifyChannels(_ channels: [Channel])
+    
+    /// Удалить каналы
+    func removeChannels(_ channels: [Channel])
+    
+    /// Удалить старты каналы
+    func removeOldChannels(_ channels: [Channel])
 }
--- fintech_chat/fintech_chat/Service Layer/Services/MessageManager/IMessageManager.swift
+++ fintech_chat/fintech_chat/Service Layer/Services/MessageManager/IMessageManager.swift
@@ -1,5 +1,6 @@
-import Foundation
+import CoreData
 
 protocol IMessageManager {
-    
+    /// Добавить новые сообщения в канал
+    func addNewMessages(_ messages: [Message], for channelId: NSManagedObjectID)
 }
