cd950379b2d659dba91f8cd46e241095bc22a753
Rabbit <dimkazayats@gmail.com>
2020-12-01 17:16:30 +0300

    Поменял имя тестов

---
+++ fintech_chat/fintech_chat Tests/ThemeManagerTests.swift
@@ -0,0 +1,25 @@
+//
+//  fintech_chat_Tests.swift
+//  fintech_chat Tests
+//
+//  Created by Admin on 12/1/20.
+//  Copyright © 2020 Admin. All rights reserved.
+//
+
+import XCTest
+
+class ThemeManagerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+}
